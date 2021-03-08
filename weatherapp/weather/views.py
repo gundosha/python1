765786915df19ca1529ab2f6cd2cc6f3d4943665
gundosha <zamilov_2016@mail.ru>
2021-03-08 19:50:42 +0300

    test
    test

---
+++ weatherapp/weather/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+import requests
+from .models import City
+from .forms import CityForm
+
+
+def index(request):
+    appid='3c2e587a730fbd90cb86c83739b69f07'
+    url='http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+    city='London'
+
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+
+
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+    city_info={
+        'city': city.name,
+        'temp': res['main']['temp'],
+        'icon': res['weather'] [0]['icon']
+    }
+    all_cities.append(city_info)
+
+    context = {'all_info': city_info, 'form': form}
+    return render(request, 'weather/index.html', context)
